d9f053a760cd4cb98e0448425f7c0b85806d0ad1
Michael <michael@mirwaldt.net>
2019-04-06 04:21:36 +0200

    code improved

--- Hacktoberfest/src/net/mirwaldt/SolveLabyrinth.java
+++ Hacktoberfest/src/net/mirwaldt/SolveLabyrinth.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -79,8 +78,7 @@ public class SolveLabyrinth {
 
 	public static void main(String[] args) throws IOException,
 			InterruptedException {
-		File outputfile = new File("D:\\saved.png");
-		BufferedImage img = ImageIO.read(outputfile);
+		BufferedImage img = ImageIO.read(SolveLabyrinth.class.getResourceAsStream("saved.png"));
 
 		int widthInPositions = 74;
 		int heightInPositions = 39;
@@ -90,33 +88,63 @@ public class SolveLabyrinth {
 
 		Field[][] map = new Field[fieldHeight][fieldWidth];
 
-		for (int fieldX = 0; fieldX < fieldWidth; fieldX++) {
-			map[0][fieldX] = Field.WALL;
-			map[fieldHeight - 1][fieldX] = Field.WALL;
-		}
+		createBorder(fieldWidth, fieldHeight, map);
 
-		for (int fieldY = 0; fieldY < fieldHeight; fieldY++) {
-			map[fieldY][0] = Field.WALL;
-			map[fieldY][fieldWidth - 1] = Field.WALL;
-		}
+		setCentralFreeFields(fieldWidth, fieldHeight, map);
 
-		for (int fieldX = 1; fieldX < fieldWidth; fieldX += 2) {
-			for (int fieldY = 1; fieldY < fieldHeight; fieldY += 2) {
-				map[fieldY][fieldX] = Field.FREE;
-			}
-		}
+		setCentralWalls(fieldWidth, fieldHeight, map);
 
-		for (int fieldX = 2; fieldX < fieldWidth; fieldX += 2) {
-			for (int fieldY = 2; fieldY < fieldHeight; fieldY += 2) {
-				map[fieldY][fieldX] = Field.WALL;
-			}
-		}
-
-		map[fieldHeight - 2][fieldWidth - 2] = Field.END;
+		setTarget(fieldWidth, fieldHeight, map);
 
 		int leftTopX = 16;
 		int leftTopY = 9;
 
+		readNonCentralWalls(img, widthInPositions, heightInPositions, map,
+				leftTopX, leftTopY);
+		System.out.println(Arrays.deepToString(map).replace("],", "],\n"));
+
+		List<Point> pathToEnd = findPath(map, new Point(1, 1),
+				new ArrayList<>(),
+				Arrays.asList(Direction.DOWN, Direction.RIGHT));
+		System.out.println(pathToEnd);
+
+		drawPath(img, leftTopX, leftTopY, pathToEnd);
+
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(new ImagePanel(img));
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(img.getWidth() + 100, img.getHeight() + 100);
+		frame.setVisible(true);
+
+		Thread.sleep(1000000);
+
+	}
+
+	private static void drawPath(BufferedImage img, int leftTopX, int leftTopY,
+			List<Point> pathToEnd) {
+		Graphics g = img.getGraphics();
+		g.setColor(Color.YELLOW);
+
+		Point previousPoint = pathToEnd.get(0);
+		for (Point point : pathToEnd.subList(1, pathToEnd.size())) {
+			int startPositionX = leftTopX + (previousPoint.getX() / 2 + 1) * 18
+					- 8;
+			int startPositionY = leftTopY + (previousPoint.getY() / 2 + 1) * 18
+					- 8;
+			int endPositionX = leftTopX + (point.getX() / 2 + 1) * 18 - 8;
+			int endPositionY = leftTopY + (point.getY() / 2 + 1) * 18 - 8;
+
+			g.drawLine(startPositionX, startPositionY, endPositionX,
+					endPositionY);
+
+			previousPoint = point;
+		}
+	}
+
+	private static void readNonCentralWalls(BufferedImage img,
+			int widthInPositions, int heightInPositions, Field[][] map,
+			int leftTopX, int leftTopY) {
 		for (int wallPositionX = 1; wallPositionX <= widthInPositions; wallPositionX++) {
 			for (int wallPositionY = 1; wallPositionY <= heightInPositions; wallPositionY++) {
 				int pixelPositionX = leftTopX + wallPositionX * 18;
@@ -141,37 +169,41 @@ public class SolveLabyrinth {
 				}
 			}
 		}
-		System.out.println(Arrays.deepToString(map).replace("],", "],\n"));
+	}
 
-		List<Point> pathToEnd = findPath(map, new Point(1, 1),
-				new ArrayList<>(),
-				Arrays.asList(Direction.DOWN, Direction.RIGHT));
-		System.out.println(pathToEnd);
+	private static void setTarget(int fieldWidth, int fieldHeight, Field[][] map) {
+		map[fieldHeight - 2][fieldWidth - 2] = Field.END;
+	}
 
-		Graphics g = img.getGraphics();
-		g.setColor(Color.YELLOW);
-		
-		Point previousPoint = pathToEnd.get(0);
-		for (Point point : pathToEnd.subList(1, pathToEnd.size())) {
-			int startPositionX = leftTopX + (previousPoint.getX() / 2 + 1)* 18 - 8;
-			int startPositionY = leftTopY + (previousPoint.getY() / 2 + 1) * 18 - 8;
-			int endPositionX = leftTopX + (point.getX() / 2 + 1) * 18 - 8;
-			int endPositionY = leftTopY + (point.getY() / 2 + 1) * 18 - 8;
+	private static void setCentralWalls(int fieldWidth, int fieldHeight,
+			Field[][] map) {
+		for (int fieldX = 2; fieldX < fieldWidth; fieldX += 2) {
+			for (int fieldY = 2; fieldY < fieldHeight; fieldY += 2) {
+				map[fieldY][fieldX] = Field.WALL;
+			}
+		}
+	}
 
-			g.drawLine(startPositionX, startPositionY, endPositionX, endPositionY);
+	private static void setCentralFreeFields(int fieldWidth, int fieldHeight,
+			Field[][] map) {
+		for (int fieldX = 1; fieldX < fieldWidth; fieldX += 2) {
+			for (int fieldY = 1; fieldY < fieldHeight; fieldY += 2) {
+				map[fieldY][fieldX] = Field.FREE;
+			}
+		}
+	}
 
-			previousPoint = point;
+	private static void createBorder(int fieldWidth, int fieldHeight,
+			Field[][] map) {
+		for (int fieldX = 0; fieldX < fieldWidth; fieldX++) {
+			map[0][fieldX] = Field.WALL;
+			map[fieldHeight - 1][fieldX] = Field.WALL;
 		}
 
-		JFrame frame = new JFrame();
-		frame.getContentPane().add(new ImagePanel(img));
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(img.getWidth() + 100, img.getHeight() + 100);
-		frame.setVisible(true);
-
-		Thread.sleep(1000000);
-
+		for (int fieldY = 0; fieldY < fieldHeight; fieldY++) {
+			map[fieldY][0] = Field.WALL;
+			map[fieldY][fieldWidth - 1] = Field.WALL;
+		}
 	}
 
 	private static List<Point> findPath(Field[][] map, Point currentPosition,
@@ -180,10 +212,7 @@ public class SolveLabyrinth {
 		if (2 <= newPosition.size()) {
 			Point first = newPosition.get(newPosition.size() - 2);
 			Point previous = newPosition.get(newPosition.size() - 1);
-			if ((first.getX() == previous.getX() && previous.getX() == currentPosition
-					.getX())
-					|| (first.getY() == previous.getY() && previous.getY() == currentPosition
-							.getY())) {
+			if (isPreviousTransitive(currentPosition, first, previous)) {
 				newPosition.remove(newPosition.size() - 1);
 			}
 		}
@@ -209,6 +238,14 @@ public class SolveLabyrinth {
 			}
 		}
 		return Collections.emptyList();
+	}
+
+	private static boolean isPreviousTransitive(Point currentPosition,
+			Point first, Point previous) {
+		return (first.getX() == previous.getX() && previous.getX() == currentPosition
+				.getX())
+				|| (first.getY() == previous.getY() && previous.getY() == currentPosition
+						.getY());
 	}
 
 	static class ImagePanel extends JPanel {
